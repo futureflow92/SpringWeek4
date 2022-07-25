c067b348aadea833b9522681c0e9168f86be343f
futureflow92 <futureflow92@hotmail.com>
2022-07-25 15:53:15 +0900

    관심 상품 등록하기를 위한 ProductRequestDto, 관심 가격 변경하기를 위한 ProductMypriceRequestDto 생성

---
+++ src/main/java/com/sparta/week04/models/ProductMypriceRequestDto.java
@@ -0,0 +1,9 @@
+package com.sparta.week04.models;
+
+import lombok.Getter;
+import org.hibernate.annotations.GeneratorType;
+
+@Getter
+public class ProductMypriceRequestDto {
+    private int myprice;
+}
---
+++ src/main/java/com/sparta/week04/models/ProductRequestDto.java
@@ -0,0 +1,16 @@
+package com.sparta.week04.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+
+public class ProductRequestDto {
+//    title, link, image, lprice
+
+
+    private String title;
+    private String link;
+    private String image;
+    private int lprice;
+}
